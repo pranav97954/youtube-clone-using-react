57f6d9094cdf89f4c40145e26f680abf460141f2
Pranav Kumar <pranavkumar97954@gmail.com>
2023-04-18 17:17:51 +0530

    Complete

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,13 +8,26 @@ import { demoThumbnailUrl, demoVideoUrl, demoChannelUrl, demoVideoTitle, demoCha
 const VideoCard = ({ video: { id: {
     videoId }, snippet } }) => {
   return (
-    <Card>
-        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+    <Card sx={{ width: { md: '320px', xs: '100%',sm:'358px' }, boxShadow:'none', borderRadius:0 }}>
+        <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY` }>
             <CardMedia 
-               image={snippet?.thumbnails?.high?.url} alt={snippet?.title} 
-               sx={{ width: 358, height: 180 }}
+              image={snippet?.thumbnails?.high?.url} alt={snippet?.title} 
+              sx={{ width: 358, height: 180 }}
             />
         </Link>
+        <CardContent sx={{ backgroundColor: '#1e1e1e',height: '106px' }}>
+          <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+              {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0,60)}
+            </Typography>
+          </Link>
+          <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+            <Typography variant="subtitle2" fontWeight="bold" color="gray">
+              {snippet?.channelTitle || demoChannelTitle}
+              <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }}/>
+            </Typography>
+          </Link>
+        </CardContent>
     </Card>
   )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import HomeIcon from '@mui/icons-material/Home';
-import CodeIcon from '@mui/icons-material/Code';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import LiveTvIcon from '@mui/icons-material/LiveTv';
@@ -16,10 +15,6 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
-  { name: 'Coding', icon: <CodeIcon />, },
-  { name: 'ReactJS', icon: <CodeIcon />, },
-  { name: 'NextJS', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
   { name: 'Education', icon: <SchoolIcon />, },
   { name: 'Podcast', icon: <GraphicEqIcon />, },
@@ -37,6 +32,6 @@ export const categories = [
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
 export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoChannelTitle = 'Pranav Kumar';
+export const demoVideoTitle = 'RapidAPI';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
